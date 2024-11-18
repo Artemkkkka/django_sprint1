bd9f9da4e9f27b64c9abfd3ca0f8cd7047ac5d04
Артём Брагин <bragin15bragin@yandex.ru>
2024-11-18 10:33:27 +0300

    Make templates for "category.html", "detail.html", "index.html"

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -44,13 +44,17 @@ posts = [
     },
 ]
 
+
 def index(request):
     template = 'blog/index.html'
+    
     return render(request, template)
+
 
 def post_detail(request):
     template = 'blog/detail.html'
     return render(request, template)
+
 
 def category_posts(request):
     template = 'blog/category.html'
