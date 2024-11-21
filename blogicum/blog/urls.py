83842e9379b654c2d18f91c8917a580eabeca839
Артём Брагин <bragin15bragin@yandex.ru>
2024-11-21 16:10:47 +0300

    edits after review

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 
 from . import views
 
-app_name = 'blog'
+app_name = "blog"
 
 urlpatterns = [
-    path("", views.index, name='index'),
-    path("<int:id>/", views.post_detail, name='post_detail'),
-    path("<slug:category_slug>/", views.category_posts, name='category_posts'),
+    path("", views.index, name="index"),
+    path("<int:id>/", views.post_detail, name="post_detail"),
+    path("<slug:category_slug>/", views.category_posts, name="category_posts"),
 ]
--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('pages/', include('pages.urls')),
-    path('', include('blog.urls')),
-    path('posts/', include('blog.urls')),
-    path('category/', include('blog.urls')),
+    path("admin/", admin.site.urls),
+    path("pages/", include("pages.urls")),
+    path("", include("blog.urls")),
+    path("posts/", include("blog.urls")),
+    path("category/", include("blog.urls")),
 ]
